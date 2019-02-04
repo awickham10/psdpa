00027d23ed276421fd2d78fae99fc251a972d9dd
Andrew Wickham <andrew@awickham.com>
2019-02-04 16:18:00 -0500

    Updated help for Get-DpaAnnotation

--- PSDPA/Public/Get-DpaAnnotation.ps1
+++ PSDPA/Public/Get-DpaAnnotation.ps1
@@ -1,3 +1,62 @@
+<#
+
+.SYNOPSIS
+Gets custom annotations from DPA.
+
+.DESCRIPTION
+Gets custom annotations from DPA for a specific monitor within an optionally
+specified time period.
+
+.PARAMETER DatabaseId
+Database ID of the monitor to get annotations for. This cannot be used in
+combination with MonitorName or Monitor.
+
+.PARAMETER MonitorName
+Name of the monitor to get annotations for. This cannot be used in combination
+with DatabaseId or Monitor.
+
+.PARAMETER Monitor
+Monitor object to get annotations for. This cannot be used in combination with
+DatabaseId or MonitorName.
+
+.PARAMETER StartTime
+The beginning of the time period to get annotations for.
+
+.PARAMETER EndTime
+The end of the time period to get annotations for.
+
+.PARAMETER EnableException
+Replaces user friendly yellow warnings with bloody red exceptions of doom! Use
+this if you want the function to throw terminating errors you want to catch.
+
+.NOTES
+Tags: Database
+Author: Andrew Wickham ( @awickham )
+
+Copyright: (C) Andrew Wickham, andrew@awickham.com
+License: MIT https://opensource.org/licenses/MIT
+
+.EXAMPLE
+Get-DpaAnnotation -DatabaseId 1
+
+Gets annotations for a specific Database ID
+
+.EXAMPLE
+Get-DpaAnnotation -MonitorName 'MyMonitoredServer'
+
+Gets annotations for a specific monitor name
+
+.EXAMPLE
+Get-DpaMonitor -MonitorName 'MyMonitoredServer' | Get-DpaAnnotation
+
+Gets annotations by piping a monitor
+
+.EXAMPLE
+Get-DpaMonitor | Get-DpaAnnotation -StartTime (Get-Date).AddDays(-30)
+
+Get all annotations over the last 30 days for all monitors
+
+#>
 function Get-DpaAnnotation {
     [CmdletBinding(DefaultParameterSetName = 'ByName')]
     param (
