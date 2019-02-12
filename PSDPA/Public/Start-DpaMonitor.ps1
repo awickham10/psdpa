a22c1d80d7394f39e1e7d5ca0c6b10790a798709
Andrew Wickham <andrew@awickham.com>
2019-02-12 15:44:07 -0500

    Added help for Start-DpaMonitor and added pipeline support

--- PSDPA/Public/Start-DpaMonitor.ps1
+++ PSDPA/Public/Start-DpaMonitor.ps1
@@ -1,35 +1,79 @@
+<#
+
+.SYNOPSIS
+Starts a DPA monitor
+
+.PARAMETER DatabaseId
+Database ID of the monitor to start. This cannot be used in combination with
+MonitorName.
+
+.PARAMETER MonitorName
+Name of the monitor to start. This cannot be used in combination with DatabaseId.
+
+.PARAMETER Monitor
+Monitor object to start. This cannot be used in combination with DatabaseId or
+MonitorName.
+
+.PARAMETER EnableException
+Replaces user friendly yellow warnings with bloody red exceptions of doom! Use
+this if you want the function to throw terminating errors you want to catch.
+
+.EXAMPLE
+Start-DpaMonitor -DatabaseId 1
+
+Starts the monitor for Database ID 1
+
+.EXAMPLE
+Start-DpaMonitor -MonitorName 'MyMonitoredServer'
+
+Starts the monitor for 'MyMonitoredServer'
+
+.NOTES
+Author: Andrew Wickham ( @awickham )
+
+Copyright: (C) Andrew Wickham, andrew@awickham.com
+License: MIT https://opensource.org/licenses/MIT
+
+#>
+
 function Start-DpaMonitor {
     [CmdletBinding(DefaultParameterSetName = 'ByName')]
     param (
         [Parameter(ParameterSetName = 'ByDatabaseId', Mandatory)]
-        $DatabaseId,
+        [int[]] $DatabaseId,
 
         [Parameter(ParameterSetName = 'ByName', Mandatory)]
-        $MonitorName,
+        [string[]] $MonitorName,
 
+        [Parameter(ParameterSetName = 'ByMonitor', ValueFromPipeline)]
+        [Monitor[]] $Monitor,
+
+        [Parameter()]
         [switch] $EnableException
     )
 
-    if ($PSCmdlet.ParameterSetName -eq 'ByName') {
-        $monitor = Get-DpaMonitor -MonitorName $MonitorName
-    }
-    else {
-        $monitor = Get-DpaMonitor -DatabaseId $DatabaseId
+
+    begin {
+        if ($PSCmdlet.ParameterSetName -eq 'ByName') {
+            $Monitor = Get-DpaMonitor -MonitorName $MonitorName
+        }
+        elseif ($PSCmdlet.ParameterSetName -eq 'ByDatabaseId') {
+            $Monitor = Get-DpaMonitor -DatabaseId $DatabaseId
+        }
+
+        $request = @{
+            command = 'START'
+        }
     }
 
-    if (-not $monitor) {
-        Stop-PSFFunction -Message "Monitor does not exist" -EnableException:$EnableException
-        return
-    }
-
-    $request = @{
-        command = 'START'
-    }
-
-    try {
-        $response = Invoke-DpaRequest -Endpoint "/databases/$($monitor.Dbid)/monitor-status" -Method 'PUT' -Request $request
-    }
-    catch {
-        Stop-PSFFunction -Message "Could not start the monitor" -ErrorRecord $_ -Target $monitor.Name
+    process {
+        foreach ($monitorObject in $monitor) {
+            try {
+                $response = Invoke-DpaRequest -Endpoint "/databases/$($monitor.Dbid)/monitor-status" -Method 'PUT' -Request $request
+            }
+            catch {
+                Stop-PSFFunction -Message "Could not start the monitor" -EnableException:$EnableException -ErrorRecord $_ -Target $monitor.Name
+            }
+        }
     }
 }
