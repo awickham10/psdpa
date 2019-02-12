642245063cbbfb53eb986cbaf7afb385c56e4327
Andrew Wickham <andrew@awickham.com>
2019-02-12 15:13:41 -0500

    Added help for Remove-DpaAnnotation

--- PSDPA/Public/Remove-DpaAnnotation.ps1
+++ PSDPA/Public/Remove-DpaAnnotation.ps1
@@ -1,3 +1,31 @@
+<#
+
+.SYNOPSIS
+Removes an annotation from DPA.
+
+.DESCRIPTION
+Takes an Annotation object and removes it from DPA.
+
+.PARAMETER Annotation
+Annotation to remove.
+
+.PARAMETER EnableException
+Replaces user friendly yellow warnings with bloody red exceptions of doom! Use
+this if you want the function to throw terminating errors you want to catch.
+
+.EXAMPLE
+Get-DpaAnnotation -MonitorName 'MyMonitoredServer' | Where-Object { $_.Title -eq 'Patching' } | Remove-DpaAnnotation
+
+Removes any annotation with a title of "Patching" from MyMonitoredServer.
+
+.NOTES
+Author: Andrew Wickham ( @awickham )
+
+Copyright: (C) Andrew Wickham, andrew@awickham.com
+License: MIT https://opensource.org/licenses/MIT
+
+#>
+
 function Remove-DpaAnnotation {
     [CmdletBinding(DefaultParameterSetName = 'ById')]
     param (
