03631e7e88280d0663528c1d804a80e1e296c8a1
Andrew Wickham <andrew@awickham.com>
2019-02-12 15:16:07 -0500

    Added help for Get-DpaConfig

--- PSDPA/Public/Get-DpaConfig.ps1
+++ PSDPA/Public/Get-DpaConfig.ps1
@@ -1,3 +1,29 @@
+<#
+
+.SYNOPSIS
+Gets a configuration option for PSDPA.
+
+.PARAMETER Name
+Name of the configuration option. Wildcarding with * is supported.
+
+.EXAMPLE
+Get-DpaConfig -Name *
+
+Gets all configuration options for PSDPA
+
+.EXAMPLE
+Get-DpaConfig -Name baseuri
+
+Gets the BaseUri configuration option for DPA
+
+.NOTES
+Author: Andrew Wickham ( @awickham )
+
+Copyright: (C) Andrew Wickham, andrew@awickham.com
+License: MIT https://opensource.org/licenses/MIT
+
+#>
+
 function Get-DpaConfig {
     [CmdletBinding()]
     param (
@@ -5,19 +31,15 @@ function Get-DpaConfig {
         [string] $Name = "*"
     )
 
-    begin {
-        $module = "psdpa"
-    }
+    $module = "psdpa"
 
-    process {
-        $Name = $Name.ToLower()
+    $Name = $Name.ToLower()
 
-        $results = [PSFramework.Configuration.ConfigurationHost]::Configurations.Values | Where-Object {
-                ($_.Name -like $Name) -and
-                ($_.Module -like $Module) -and
-                ((-not $_.Hidden) -or ($Force))
-            } | Sort-Object Module, Name
+    $results = [PSFramework.Configuration.ConfigurationHost]::Configurations.Values | Where-Object {
+            ($_.Name -like $Name) -and
+            ($_.Module -like $Module) -and
+            ((-not $_.Hidden) -or ($Force))
+        } | Sort-Object Module, Name
 
-        $results | Select-Object Name, Value, Description
-    }
+    $results | Select-Object Name, Value, Description
 }
