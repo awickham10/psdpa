69ab7d4508b5bb425143cdb45e647ee0de8bc229
Andrew Wickham <andrew@awickham.com>
2019-02-12 15:46:42 -0500

    Added help for Stop-DpaMonitor and added pipeline support

--- PSDPA/Public/Stop-DpaMonitor.ps1
+++ PSDPA/Public/Stop-DpaMonitor.ps1
@@ -1,38 +1,80 @@
-function Stop-DpaMonitor {
-    [CmdletBinding(DefaultParameterSetName = 'ByName', SupportsShouldProcess, ConfirmImpact = 'High')]
+<#
+
+.SYNOPSIS
+Stops a DPA monitor
+
+.PARAMETER DatabaseId
+Database ID of the monitor to stop. This cannot be used in combination with
+MonitorName.
+
+.PARAMETER MonitorName
+Name of the monitor to stop. This cannot be used in combination with DatabaseId.
+
+.PARAMETER Monitor
+Monitor object to stop. This cannot be used in combination with DatabaseId or
+MonitorName.
+
+.PARAMETER EnableException
+Replaces user friendly yellow warnings with bloody red exceptions of doom! Use
+this if you want the function to throw terminating errors you want to catch.
+
+.EXAMPLE
+Stop-DpaMonitor -DatabaseId 1
+
+Stops the monitor for Database ID 1
+
+.EXAMPLE
+Stop-DpaMonitor -MonitorName 'MyMonitoredServer'
+
+Stops the monitor for 'MyMonitoredServer'
+
+.NOTES
+Author: Andrew Wickham ( @awickham )
+
+Copyright: (C) Andrew Wickham, andrew@awickham.com
+License: MIT https://opensource.org/licenses/MIT
+
+#>
+
+function Start-DpaMonitor {
+    [CmdletBinding(DefaultParameterSetName = 'ByName')]
     param (
         [Parameter(ParameterSetName = 'ByDatabaseId', Mandatory)]
-        $DatabaseId,
+        [int[]] $DatabaseId,
 
         [Parameter(ParameterSetName = 'ByName', Mandatory)]
-        $MonitorName,
+        [string[]] $MonitorName,
+
+        [Parameter(ParameterSetName = 'ByMonitor', ValueFromPipeline)]
+        [Monitor[]] $Monitor,
 
         [Parameter()]
         [switch] $EnableException
     )
 
-    if ($PSCmdlet.ParameterSetName -eq 'ByName') {
-        $monitor = Get-DpaMonitor -MonitorName $MonitorName
-    }
-    else {
-        $monitor = Get-DpaMonitor -DatabaseId $DatabaseId
-    }
-
-    if (-not $monitor) {
-        Stop-PSFFunction -Message "Monitor does not exist" -EnableException:$EnableException
-        return
-    }
-
-    $request = @{
-        command = 'STOP'
-    }
-
-    if ($PSCmdlet.ShouldProcess($monitor.Name, 'Stop Monitor')) {
-        try {
-            $response = Invoke-DpaRequest -Endpoint "/databases/$($monitor.Dbid)/monitor-status" -Method 'PUT' -Request $request
+    begin {
+        if ($PSCmdlet.ParameterSetName -eq 'ByName') {
+            $Monitor = Get-DpaMonitor -MonitorName $MonitorName
         }
-        catch {
-            Stop-PSFFunction -Message "Could not stop the monitor" -ErrorRecord $_ -Target $monitor.Name
+        elseif ($PSCmdlet.ParameterSetName -eq 'ByDatabaseId') {
+            $Monitor = Get-DpaMonitor -DatabaseId $DatabaseId
+        }
+
+        $request = @{
+            command = 'STOP'
+        }
+    }
+
+    process {
+        foreach ($monitorObject in $Monitor) {
+            if ($PSCmdlet.ShouldProcess($monitor.Name, 'Stop Monitor')) {
+                try {
+                    $response = Invoke-DpaRequest -Endpoint "/databases/$($monitorObject.Dbid)/monitor-status" -Method 'PUT' -Request $request
+                }
+                catch {
+                    Stop-PSFFunction -Message "Could not stop the monitor" -ErrorRecord $_ -Target $monitorObject.Name
+                }
+            }
         }
     }
 }
