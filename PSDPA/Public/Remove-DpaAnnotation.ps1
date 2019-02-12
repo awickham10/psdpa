2207076e0c9711689857f1b40b72a31a8129bc03
Andrew Wickham <andrew@awickham.com>
2019-02-12 15:10:55 -0500

    Initial draft of remove annotation

--- PSDPA/Public/Remove-DpaAnnotation.ps1
+++ PSDPA/Public/Remove-DpaAnnotation.ps1
@@ -1,6 +1,20 @@
 function Remove-DpaAnnotation {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName = 'ById')]
     param (
+        [Parameter(ParameterSetName = 'ByObject', ValueFromPipeline)]
+        [Annotation[]] $Annotation,
 
+        [Parameter()]
+        [switch] $EnableException
     )
+
+    foreach ($annotationObject in $Annotation) {
+        $endpoint = "databases/$($annotationObject.DatabaseId)/annotations/$($annotationObject.AnnotationId)"
+        try {
+            $response = Invoke-DpaRequest -Endpoint $endpoint -Method 'Delete'
+        }
+        catch {
+            Stop-PSFFunction -Message "Could not remove annotation" -EnableException:$EnableException -ErrorRecord $_
+        }
+    }
 }
