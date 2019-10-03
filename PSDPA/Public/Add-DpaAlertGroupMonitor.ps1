07d600e528fc1482e979d0cb5a036af377d09d61
Andrew Wickham <awickham10@users.noreply.github.com>
2019-10-03 07:01:10 -0400

    Add monitor to alert group

---
+++ PSDPA/Public/Add-DpaAlertGroupMonitor.ps1
@@ -0,0 +1,40 @@
+function Add-DpaAlertGroupMonitor {
+    [CmdletBinding(DefaultParameterSetName = 'ByName')]
+    param (
+        [Parameter(ParameterSetName = 'ByDatabaseId', Mandatory)]
+        [int[]] $DatabaseId,
+
+        [Parameter(ParameterSetName = 'ByName', Mandatory)]
+        [string[]] $MonitorName,
+
+        [Parameter(ParameterSetName = 'ByMonitor', ValueFromPipeline)]
+        [Monitor[]] $Monitor,
+
+        [Parameter(Mandatory)]
+        [AlertGroup] $AlertGroup,
+
+        [Parameter()]
+        [switch] $EnableException
+    )
+
+    begin {
+        if ($PSCmdlet.ParameterSetName -eq 'ByName') {
+            $Monitor = Get-DpaMonitor -MonitorName $MonitorName
+        } elseif ($PSCmdlet.ParameterSetName -eq 'ByDatabaseId') {
+            $Monitor = Get-DpaMonitor -DatabaseId $DatabaseId
+        }
+    }
+
+    process {
+        foreach ($monitorObject in $Monitor) {
+            Write-PSFMessage -Level Verbose -Message "Adding Database ID $($monitorObject.DatabaseId) to Alert Group ID $($AlertGroup.AlertGroupId)"
+            $endpoint = "/alert-groups/$($AlertGroup.AlertGroupId)/databases/$($monitorObject.DatabaseId)"
+
+            try {
+                $null = Invoke-DpaRequest -Endpoint $endpoint -Method 'Post'
+            } catch {
+                Stop-PSFFunction -Level Critical -Message 'Could not associate monitor to alert group' -ErrorRecord $_ -EnableException $EnableException
+            }
+        }
+    }
+}
