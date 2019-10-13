dcdc20d851a814c5c9692927227cbd0739666e41
Andrew Wickham <awickham10@users.noreply.github.com>
2019-10-13 11:07:25 -0400

    Allow getting monitors by piped alert group

--- PSDPA/Public/Get-DpaMonitor.ps1
+++ PSDPA/Public/Get-DpaMonitor.ps1
@@ -45,37 +45,52 @@ function Get-DpaMonitor {
         [ValidateNotNullOrEmpty()]
         [string[]] $MonitorName,
 
+        [Parameter(ParameterSetName = 'Pipeline', ValueFromPipeline)]
+        [object[]] $InputObject,
+
         [Parameter()]
         [switch] $EnableException
     )
 
-    if ($PSBoundParameters.ContainsKey('DatabaseId') -and $DatabaseId.Count -eq 1) {
-        Write-PSFMessage -Level Verbose -Message 'Getting a single monitor'
-        $endpoint = "/databases/$DatabaseId/monitor-information"
-    } else {
-        Write-PSFMessage -Level Verbose -Message 'Getting all monitors'
-        $endpoint = '/databases/monitor-information'
-    }
-
-    try {
-        $response = Invoke-DpaRequest -Endpoint $endpoint -Method 'Get'
-        $monitors = $response.data
-    } catch {
-        if ($_.Exception.Response.StatusCode.value__ -eq 422) {
-            return $null
+    process {
+        if ($PSCmdlet.ParameterSetName -eq 'Pipeline') {
+            foreach ($iObject in $InputObject) {
+                switch ($iObject.GetType().Name) {
+                    'AlertGroup' {
+                        $iObject.Monitors
+                    }
+                }
+            }
+        } else {
+            if ($PSBoundParameters.ContainsKey('DatabaseId') -and $DatabaseId.Count -eq 1) {
+                Write-PSFMessage -Level Verbose -Message 'Getting a single monitor'
+                $endpoint = "/databases/$DatabaseId/monitor-information"
+            } else {
+                Write-PSFMessage -Level Verbose -Message 'Getting all monitors'
+                $endpoint = '/databases/monitor-information'
+            }
+        
+            try {
+                $response = Invoke-DpaRequest -Endpoint $endpoint -Method 'Get'
+                $monitors = $response.data
+            } catch {
+                if ($_.Exception.Response.StatusCode.value__ -eq 422) {
+                    return $null
+                }
+        
+                Stop-PSFFunction -Message 'Could not retrieve monitor information' -ErrorRecord $_ -EnableException:$EnableException
+            }
+        
+            if ($PSBoundParameters.ContainsKey('DatabaseId') -and $DatabaseId -is [array]) {
+                $monitors = $monitors | Where-Object { $_.dbid -in $DatabaseId }
+            } elseif ($PSCmdlet.ParameterSetName -eq 'ByName') {
+                $monitors = $monitors | Where-Object { $_.name -in $MonitorName }
+            }
+        
+            $monitorFactory = New-Object MonitorFactory
+            foreach ($monitor in $monitors) {
+                $monitorFactory.New($monitor)
+            }
         }
-
-        Stop-PSFFunction -Message 'Could not retrieve monitor information' -ErrorRecord $_ -EnableException:$EnableException
-    }
-
-    if ($PSBoundParameters.ContainsKey('DatabaseId') -and $DatabaseId -is [array]) {
-        $monitors = $monitors | Where-Object { $_.dbid -in $DatabaseId }
-    } elseif ($PSCmdlet.ParameterSetName -eq 'ByName') {
-        $monitors = $monitors | Where-Object { $_.name -in $MonitorName }
-    }
-
-    $monitorFactory = New-Object MonitorFactory
-    foreach ($monitor in $monitors) {
-        $monitorFactory.New($monitor)
     }
 }
